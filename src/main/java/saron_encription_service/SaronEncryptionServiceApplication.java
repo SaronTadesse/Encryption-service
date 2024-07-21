81c5d9c1d41209e602ce8114b0d3900ac71ed7db
sarontadesse <sarontadesse456@gmail.com>
2024-07-20 22:36:35 -0500

    initial commit

---
+++ src/main/java/saron_encription_service/SaronEncryptionServiceApplication.java
@@ -0,0 +1,13 @@
+package saron_encription_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronEncryptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronEncryptionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saron_encription_service/SaronEncryptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saron_encription_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronEncryptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
